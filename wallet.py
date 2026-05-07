292c0c8d731afbcd5586db16a03c7f035d264c89
layansalym-creator <layansalym@gmail.com>
2026-05-07 09:16:21 +0300

    Add files via upload

--- wallet.py
+++ wallet.py
@@ -1,30 +1,44 @@
 import hashlib
 import random
-
+import csv
 
 def generate_wallet(seed):
     random.seed(seed)
-    return random.getrandbits(256)
 
+    # weak predictable key
+    return random.randint(1000, 9999)
 
 def generate_public_key(private_key):
     return hashlib.sha256(str(private_key).encode()).hexdigest()
-
 
 def generate_wallet_address(public_key):
     wallet_address = hashlib.sha256(public_key.encode()).hexdigest()[:32]
     return "WALLET-" + wallet_address
 
 
+with open("data.csv", "w", newline="") as file:
+
+    writer = csv.writer(file)
+
+    writer.writerow(["seed", "private_key"])
+
+    for seed in range(1000):
+
+        private_key = generate_wallet(seed)
+
+        writer.writerow([seed, private_key])
 
 
 seed = 42
+
 private_key = generate_wallet(seed)
+
 public_key = generate_public_key(private_key)
+
 wallet_address = generate_wallet_address(public_key)
 
-if __name__ == "__main__":
-    print(f"Seed: {seed}")
-    print(f"Private Key (int): {private_key}")
-    print(f"Public Key: {public_key}")
-    print(f"Wallet Address: {wallet_address}")
+print(f"Seed: {seed}")
+
+print(f"Private Key: {private_key}")
+
+print(f"Wallet Address: {wallet_address}")
